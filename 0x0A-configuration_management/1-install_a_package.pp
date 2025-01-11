8e96d109158ab2ecb8c24ce441d17faa6afc953b
johnkenzzy <johnkenumeh@gmail.com>
2025-01-11 05:06:43 +0100

    Flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,10 @@
-# Ensure pip3 is installed
+# This manifest ensure pip3 is installed
 package { 'python3-pip':
   ensure => present,
 }
 
 # This manifest installs flask vision 2.1.0 from pip3
-package { 'flask':
+package { 'Flask':
   ensure          => '2.1.0',
   provider        => 'pip',
   install_options => ['--pip-command', '/usr/bin/pip3'],
