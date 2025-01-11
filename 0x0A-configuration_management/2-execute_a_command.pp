225c2ca97ae9f49f3573a6a1aa21615dda886c64
johnkenzzy <johnkenumeh@gmail.com>
2025-01-11 04:53:06 +0100

    Task 2: puppet exec resource

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,6 @@
+# This manifest kills a process named killmenow
+exec { 'kill_killmenow_process':
+  command => '/usr/bin/pkill killmenow',
+  onlyif  => '/usr/bin/pgrep killmenow',
+  path    => ['/usr/bin', '/bin'],
+}
