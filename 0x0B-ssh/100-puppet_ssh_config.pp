f5671ba153872931dc797ceb396a200951aee238
johnkenzzy <johnkenumeh@gmail.com>
2025-01-12 04:25:47 +0100

    File_line

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -9,9 +9,26 @@ file { '/root/.ssh':
 # Manage the SSH client configuration file
 file { '/root/.ssh/config':
   ensure  => file,
-  content => "Host *\n    PasswordAuthentication no\n    IdentityFile ~/.ssh/school\n",
   owner   => 'root',
   group   => 'root',
   mode    => '0600',
   require => File['/root/.ssh'],
+}
+
+# Add the configuration to refuse password authentication
+file_line { 'Turn off passwd auth':
+  ensure  => present,
+  path    => '/root/.ssh/config',
+  line    => '    PasswordAuthentication no',
+  match   => '^\s*PasswordAuthentication',  # Update if it already exists
+  require => File['/root/.ssh/config'],
+}
+
+# Add the configuration to use the private key
+file_line { 'Declare identity file':
+  ensure  => present,
+  path    => '/root/.ssh/config',
+  line    => '    IdentityFile ~/.ssh/school',
+  match   => '^\s*IdentityFile', # Update if it exits already
+  require => File['/root/.ssh/config'],
 }
