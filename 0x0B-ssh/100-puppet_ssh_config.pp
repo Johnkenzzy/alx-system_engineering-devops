2600e88f1eb305b017cb388ccfa3192fb05e87eb
johnkenzzy <johnkenumeh@gmail.com>
2025-01-12 05:11:43 +0100

    Change approach

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,34 +1,20 @@
-# Ensure the .ssh directory exists with correct permissions
-file { '/root/.ssh':
-  ensure => directory,
-  owner  => 'root',
-  group  => 'root',
-  mode   => '0700',
+# SSH client configuration: Disable password authentication
+exec { 'Turn off passwd auth':
+  command => 'bash -c "echo PasswordAuthentication no >> /etc/ssh/ssh_config"',
+  path    => '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
+  unless  => 'grep -q "^PasswordAuthentication no$" /etc/ssh/ssh_config',
 }
 
-# Manage the SSH client configuration file
-file { '/root/.ssh/config':
-  ensure  => file,
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0600',
-  require => File['/root/.ssh'],
+# Set private key file location
+exec { 'Declare identity file':
+  command => 'bash -c "echo IdentityFile \'~/.ssh/school\' >> /etc/ssh/ssh_config"',
+  path    => '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
+  unless  => 'grep -q "^IdentityFile ~/.ssh/school$" /etc/ssh/ssh_config',
 }
 
-# Add the configuration to refuse password authentication
-file_line { 'Turn off passwd auth':
-  ensure  => present,
-  path    => '/root/.ssh/config',
-  line    => '    PasswordAuthentication no',
-  match   => '^\s*PasswordAuthentication',  # Update if it already exists
-  require => File['/root/.ssh/config'],
-}
-
-# Add the configuration to use the private key
-file_line { 'Declare identity file':
-  ensure  => present,
-  path    => '/root/.ssh/config',
-  line    => '    IdentityFile ~/.ssh/school',
-  match   => '^\s*IdentityFile', # Update if it exits already
-  require => File['/root/.ssh/config'],
+# Enable public key authentication
+exec { 'Turn on pubkey auth':
+  command => 'bash -c "echo PubkeyAuthentication yes >> /etc/ssh/ssh_config"',
+  path    => '/usr/bin/sbin:bin:/usr/local/bin',
+  unless  => 'grep -q "^PubkeyAuthentication yes$" /etc/ssh/ssh_config',
 }
