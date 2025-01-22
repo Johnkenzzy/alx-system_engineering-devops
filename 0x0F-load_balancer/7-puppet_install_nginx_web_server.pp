e58b47495e4fe9d6b0de952788c980fb1300852b
johnkenzzy <johnkenumeh@gmail.com>
2025-01-22 05:49:21 +0100

    Fix

---
+++ 0x0F-load_balancer/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,29 @@
+# Puppet manifest to install and configure Nginx with the specified requirements
+
+# Ensure the system is updated and Nginx is installed
+exec { 'update-apt':
+  command => '/usr/bin/apt-get update -y',
+  path    => ['/usr/bin', '/usr/sbin'],
+  unless  => '/usr/bin/test -f /var/lib/apt/periodic/update-success-stamp',
+}
+
+package { 'nginx':
+  ensure  => installed,
+  require => Exec['update-apt'],
+}
+
+# Add the custom HTTP header to Nginx configuration
+file_line { 'add_custom_http_header':
+  path    => '/etc/nginx/nginx.conf',
+  match   => '^http {',
+  line    => "http {\n    add_header X-Served-By \"${facts['networking']['hostname']}\";",
+  require => Package['nginx'],
+}
+
+# Restart Nginx service to apply changes
+service { 'nginx':
+  ensure    => running,
+  enable    => true,
+  subscribe => File_line['add_custom_http_header'],
+}
+
