ea82ee532c405fcb0226c611a2adc21ef99b0b4c
johnkenzzy <johnkenumeh@gmail.com>
2025-01-22 05:53:47 +0100

    Fix

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,28 +1,17 @@
 # Puppet manifest to configure Nginx with a custom HTTP header
 
 # Ensure the system is updated and Nginx is installed
-exec { 'update-apt':
-  command => '/usr/bin/apt-get update -y',
-  path    => ['/usr/bin', '/usr/sbin'],
-  unless  => '/usr/bin/test -f /var/lib/apt/periodic/update-success-stamp',
+exec {'update':
+  command => '/usr/bin/apt-get update',
 }
-
-package { 'nginx':
-  ensure  => installed,
-  require => Exec['update-apt'],
+-> package {'nginx':
+  ensure => 'present',
 }
-
-# Add the custom HTTP header to Nginx configuration
-file_line { 'add_custom_http_header':
-  path    => '/etc/nginx/nginx.conf',
-  match   => '^http {',
-  line    => "http {\n    add_header X-Served-By \"${facts['networking']['hostname']}\";",
-  require => Package['nginx'],
+-> file_line { 'http_header':
+  path  => '/etc/nginx/nginx.conf',
+  match => 'http {',
+  line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
 }
-
-# Restart Nginx service to apply changes
-service { 'nginx':
-  ensure    => running,
-  enable    => true,
-  subscribe => File_line['add_custom_http_header'],
+-> exec {'run':
+  command => '/usr/sbin/service nginx restart',
 }
