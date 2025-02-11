99889d72bbd41306fe3986d17fbc7e91dfc12ebb
johnkenzzy <johnkenumeh@gmail.com>
2025-02-11 04:18:05 +0100

    Optimization

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -18,13 +18,11 @@ if __name__ == '__main__':
         todos = todos_resp.json()
         name = user['name']
         total = len(todos)
-        completed = 0
-        for todo in todos:
-            if todo['completed']:
-                completed += 1
-        print(f'Employee {name} is done with tasks({completed}/{total}):')
+        completed_todos = [todo for todo in todos if todo['completed']]
+        completed = len(completed_todos)
 
-        for todo in todos:
-            print(f"     {todo['title']}")
+        print(f'Employee {name} is done with tasks({completed}/{total}):')
+        for todo in completed_todos:
+            print(f"\t {todo['title']}")
     except Exception as e:
         print(f'Error: {e}')
