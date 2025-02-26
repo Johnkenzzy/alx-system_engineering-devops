3b5897aaaffcd1c6abd572756c85ff30614d4aa2
johnkenzzy <johnkenumeh@gmail.com>
2025-02-26 02:07:07 +0100

    Fix check

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,15 +5,16 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """Returns the number of subscribers (not active and total subscribers)"""
-
+    """Returns the number of subscribers"""
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     headers = {"User-Agent": "ALX"}
 
     try:
-        subreddit = requests.get(url, headers=headers)
-        if subreddit.status_code == 200:
-            subreddit = subreddit.json()
+        resp = requests.get(url, headers=headers)
+        if resp.status_code == 200:
+            subreddit = resp.json()
             return subreddit['data']['subscribers']
+        else:
+            return 0
     except Exception as e:
         return 0
