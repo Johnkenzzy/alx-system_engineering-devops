f65adc32c9f65dce23593dabad7ae18ebc04952b
johnkenzzy <johnkenumeh@gmail.com>
2025-02-26 03:40:04 +0100

    Complete task 2

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Defines recurse function"""
+import json
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=None):
+    """Return a list all hot article titles of a given subreddit"""
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json'
+    headers = {"User-Agent": "ALX"}
+    if after:
+        url += f'?after={after}'
+
+    headers = {"User-Agent": "ALX"}
+
+    try:
+        resp = requests.get(url, headers=headers)
+        if resp.status_code == 200:
+            data = resp.json()
+            posts = data['data']['children']
+            for post in posts:
+                hot_list.append(post['data']['title'])
+
+            after = data['data']['after']
+            if after:
+                return recurse(subreddit, hot_list, after)
+            return hot_list
+        else:
+            return None
+    except Exception as e:
+        return None
