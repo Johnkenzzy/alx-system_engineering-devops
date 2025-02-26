51993b1d279ea8e1b3702c91daab99975d0cf8ed
johnkenzzy <johnkenumeh@gmail.com>
2025-02-26 01:38:37 +0100

    Complete task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Defines number_of_subscribers function"""
+import json
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Returns the number of subscribers (not active and total subscribers)"""
+
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    headers = {"User-Agent": "ALX"}
+
+    try:
+        subreddit = requests.get(url, headers=headers)
+        if subreddit.status_code == 200:
+            subreddit = subreddit.json()
+            return subreddit['data']['subscribers']
+    except Exception as e:
+        return 0
